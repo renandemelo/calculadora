7c70552094769e62b1a7307f7413d82d18236937
Renan Oliveira <renandemelo@gmail.com>
2014-04-04 13:46:39 -0300

    alterando valor inicial para 0

--- calculadora.rb
+++ calculadora.rb
@@ -1,7 +1,7 @@
 class Calculadora
 
 	def fibonacci(numero)
-		return 1 if( numero == 1 or numero == 2)
+		return 0 if( numero == 1 or numero == 2)
 		return fibonacci(numero - 1) + fibonacci(numero - 2)
 	end
 
