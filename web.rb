c208519e524da9719703bcc5e514fbeecedb8bea
Renan Oliveira <renandemelo@gmail.com>
2014-04-04 04:02:02 -0300

    alterando fibonacci

--- web.rb
+++ web.rb
@@ -4,5 +4,5 @@ require './calculadora.rb'
 get '/fibo' do
 	numero = params[:numero].to_i
 	c = Calculadora.new
-	"Fibonacci de #{numero} eh: #{c.fibonacci(numero)}"
+	"Fibonacci -- #{numero} eh: #{c.fibonacci(numero)}"
 end
